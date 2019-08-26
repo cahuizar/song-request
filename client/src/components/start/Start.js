5ce9f3283e595b81a4a936a28a1bf745ec660802
= <hire@carloshuizar.com>
2019-08-25 22:40:06 -0400

    add start component

---
+++ client/src/components/start/Start.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Figures from '../pages/Figures';
+
+const Start = () => {
+  return (
+    <div className='home'>
+      <Figures />
+      <div className=''>
+        <div>
+          <h3>Hello</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
